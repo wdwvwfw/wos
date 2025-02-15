6e2ce7cb4c621482a7192790ec7fe9d5435da1a4
wdwvwfw <3920675298@qq.com>
2025-02-15 08:55:50 +0800

    忍者传奇.lua

---
+++ 忍者传奇.lua
@@ -0,0 +1,12 @@
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/VeaMSRZK"))()
+local LBLG = Instance.new("ScreenGui", getParent)
+local LBL = Instance.new("TextLabel", getParent)
+local player = game.Players.LocalPlayer
+
+LBLG.Name = "LBLG"
+LBLG.Parent = game.CoreGui
+LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+LBLG.Enabled = true
+LBL.Name = "LBL"
+LBL.Parent = LBLG
+LBL.BackgroundColor3 = Color3.new(1, 1, 1)
