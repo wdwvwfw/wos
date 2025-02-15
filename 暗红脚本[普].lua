dc71ad94da44da52fe257c8b26f20f1fa4d6ef1b
wdwvwfw <3920675298@qq.com>
2025-02-15 09:41:02 +0800

    Update 暗红脚本[普].lua

--- 暗红脚本[普].lua
+++ 暗红脚本[普].lua
@@ -44,19 +44,12 @@ OrionLib:MakeNotification({
                     Time = 2
                 })
                 
-		local vu = game:GetService("VirtualUser")
-		game:GetService("Players").LocalPlayer.Idled:connect(function()
-		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-		   wait(1)
-		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-		end)
-
 local vu = game:GetService("VirtualUser")
 		game:GetService("Players").LocalPlayer.Idled:connect(function()
 		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 		   wait(1)
 		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-		end)      
+		end)		
 
 OrionLib:MakeNotification({
                     Name = "开启成功",
