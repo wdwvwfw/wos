9eed9e61a224805100036e57ccd914f46a8caa2e
wdwvwfw <3920675298@qq.com>
2025-02-16 08:19:36 +0800

    更新 暗红脚本[普].lua

--- 暗红脚本[普].lua
+++ 暗红脚本[普].lua
@@ -49,7 +49,7 @@ local vu = game:GetService("VirtualUser")
 		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 		   wait(1)
 		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-		end)		
+		end)
 
 OrionLib:MakeNotification({
                     Name = "开启成功",
