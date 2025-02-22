287fe0377e8dca889e9bad53715a628c6ce1dd83
CaiLun33 <caizhuch1199w@qq.com>
2025-02-22 14:11:10 +0800

    更新 幽灵透视.lua

--- 幽灵透视.lua
+++ 幽灵透视.lua
@@ -1,42 +1,28 @@
-local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/VeaMSRZK", true))()
- local Window =   OrionLib:MakeWindow({Name = "暗红血缝合王V2", HidePremium = false, SaveConfig = false, ConfigFolder = "rbxassetid://6026568198"}) 
+local GhostESPEnabled = false  -- 用来追踪幽灵透视的状态
 
- OrionLib:MakeNotification({ 
- Name = "暗红血缝合王V2", 
- Content = "欢迎使用暗红血缝合王！", 
- Image = "rbxassetid://4483345998", 
- Time = 5 
- })
+-- 创建一个开关按钮，允许玩家启用或禁用幽灵透视
+local GhostESP = Function:CreateToggle({
+    Name = "幽灵透视",
+    CurrentValue = false,
+    Flag = "ToggleGhostESP",
+    Callback = function(Value)
+        if Value then
+            GhostESPEnabled = true
+            EnableGhostESP()  -- 启用幽灵透视
+        else
+            GhostESPEnabled = false
+            DisableGhostESP()  -- 禁用幽灵透视
+        end
+    end,
+})
 
-local Players = game:GetService("Players")
-local XA = Players.LocalPlayer
-
- local LOL = Window:MakeTab({ 
- Name = "关于", 
- Icon = "rbxassetid://6026568198", 
- PremiumOnly = false 
- })
-
-LOL:AddLabel("感谢清风、꒱等人")
-LOL:AddLabel("感谢清风、꒱等人")
-LOL:AddLabel("感谢清风、꒱等人")
-LOL:AddLabel("当前服务器ID:" .. game.GameId .. ".")
-LOL:AddParagraph("您的用户名:"," "..game.Players.LocalPlayer.Name.."")
-LOL:AddParagraph("您的注入器:"," "..identifyexecutor().."")
-LOL:AddLabel("作者：暗红血缝合王")
-LOL:AddLabel("QQ：3920675298")
-LOL:AddLabel("暗红血群号：942272935")
-
-local Tab = Window:MakeTab({ 
- Name = "通用脚本", 
- Icon = "rbxassetid://10888331510", 
- PremiumOnly = false 
- })
-
-Tab:AddButton({
-	Name = "幽灵透视",
-	Callback = function()
-     for _, part in pairs(character:GetChildren()) do
+-- 启用幽灵透视：改变玩家和物体的透明度
+function EnableGhostESP()
+    local player = game.Players.LocalPlayer
+    local character = player.Character or player.CharacterAdded:Wait()
+    
+    -- 修改玩家自身的透明度，使其变得透明
+    for _, part in pairs(character:GetChildren()) do
         if part:IsA("BasePart") then
             part.LocalTransparencyModifier = 0.5  -- 设置角色部件为半透明
         end
@@ -58,18 +44,16 @@ function DisableGhostESP()
     -- 恢复玩家的透明度为不透明
     for _, part in pairs(character:GetChildren()) do
         if part:IsA("BasePart") then
-            part.LocalTransparencyModifier = 1  -- 恢复不透明
+            part.LocalTransparencyModifier = 0  -- 恢复不透明
         end
     end
 
     -- 恢复游戏世界中所有物体的透明度为不透明
     for _, obj in pairs(workspace:GetChildren()) do
         if obj:IsA("BasePart") then
-            obj.LocalTransparencyModifier = 1  -- 恢复物体为不透明
+            obj.LocalTransparencyModifier = 0  -- 恢复物体为不透明
+            end
         end
     end
 end
-  	end    
 })
-
-  	
