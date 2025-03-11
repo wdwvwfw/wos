6446d96d3d1939b9da7e58d1042c06194d6a1fbb
CaiLun33 <caizhuch1199w@qq.com>
2025-03-11 11:38:16 +0800

    更新 我的世界非roblox脚本

---
+++ 我的世界非roblox脚本/配套文件.lua
@@ -0,0 +1,15 @@
+{
+  "memory_safety": {
+    "max_scan_range": 1048576,
+    "allow_crash_recovery": true
+  },
+  "hotkey_profiles": [
+    {
+      "name": "创造模式增强",
+      "bindings": [
+        {"key": "F1", "action": "god_mode"},
+        {"key": "F4", "action": "fly_mode"}
+      ]
+    }
+  ]
+}
